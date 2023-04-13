26352fa57295721dd5b13530a97bfe318d6c938f
kevin2-cyber <keduful@st.ug.edu.gh>
2023-04-13 15:23:41 +0000

    initial commit

---
+++ backend/hms/src/test/java/com/rakeab/hms/HmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakeab.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
